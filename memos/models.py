9778a81e613e060c770617aa5b074fe64e544180
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:19:35 +0900

    feat: Add memo model PLANA-27

--- memos/models.py
+++ memos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class MemoSet(CommonModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="memo_set")
+    title = models.CharField(max_length=50)
+
+
+class Memo(CommonModel):
+    calendar = models.OneToOneField("calendars.Calendar", on_delete=models.CASCADE, related_name="memo")
+    todo = models.OneToOneField("todos.Todo", on_delete=models.CASCADE, related_name="memo")
+    title = models.CharField(default="새로운 메모")
+    text = models.TextField(null=True, blank=True)
+    is_cal = models.BooleanField(default=False)
+    is_todo = models.BooleanField(default=False)
