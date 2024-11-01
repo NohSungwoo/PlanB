c00e03547d8c5febc2b6a39fe04940a0816bf880
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:19:05 +0900

    feat: Add calendar model PLANA-28

--- calendars/models.py
+++ calendars/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Calendar(CommonModel):
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="calendar")
+
+
+class Schedule(CommonModel):
+    calendar = models.ForeignKey("calendars.Calendar", on_delete=models.CASCADE, related_name="schedule")
+    participant = models.ManyToManyField("users.User", related_name="schedule")
+    title = models.CharField(max_length=50)
+    memo = models.OneToOneField("memos.Memo", on_delete=models.CASCADE)
+    google_url = models.URLField(max_length=255, null=True)
+    start_date = models.DateField()
+    start_time = models.TimeField()
+    end_date = models.DateField()
+    end_time = models.TimeField()
+    is_repeat = models.BooleanField()
