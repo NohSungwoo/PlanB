842bfa5cd9382fad0044ed81e9e20e9f017fde84
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:59:08 +0900

    fix: update Memo Model

--- memos/models.py
+++ memos/models.py
@@ -8,9 +8,7 @@ class MemoSet(CommonModel):
 
 
 class Memo(CommonModel):
-    calendar = models.OneToOneField("calendars.Calendar", on_delete=models.CASCADE, related_name="calendar_memo")
-    todo = models.OneToOneField("todos.Todo", on_delete=models.CASCADE, related_name="todo_memo")
+    calendar = models.OneToOneField("calendars.Calendar", on_delete=models.CASCADE, null=True, related_name="calendar_memo")
+    todo = models.OneToOneField("todos.Todo", on_delete=models.CASCADE, null=True, related_name="todo_memo")
     title = models.CharField(max_length=50 ,default="새로운 메모")
     text = models.TextField(null=True, blank=True)
-    is_cal = models.BooleanField(default=False)
-    is_todo = models.BooleanField(default=False)
