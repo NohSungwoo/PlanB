e9a5150b49cc478794ab8949ec3877148596e673
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:19:52 +0900

    feat: Add todo model PLANA-29

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class TodoSet(CommonModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="todo_set")
+    title = models.CharField(max_length=50)
+
+
+class Todo(CommonModel):
+    memo = models.OneToOneField("memos.Memo", on_delete=models.CASCADE, related_name="todo")
+    tags = models.ManyToManyField("tags.Tag", related_name="todo")
+    title = models.CharField(max_length=50)
+    start_date = models.DateTimeField()
+    complete_date = models.DateTimeField(null=True, blank=True)
