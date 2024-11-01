06f5e9f4c8edac81fae86018f06edc0271543695
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:39:48 +0900

    refactor: Update memo model PLANA-27 add maxlength

--- memos/models.py
+++ memos/models.py
@@ -10,7 +10,7 @@ class MemoSet(CommonModel):
 class Memo(CommonModel):
     calendar = models.OneToOneField("calendars.Calendar", on_delete=models.CASCADE, related_name="calendar_memo")
     todo = models.OneToOneField("todos.Todo", on_delete=models.CASCADE, related_name="todo_memo")
-    title = models.CharField(default="새로운 메모")
+    title = models.CharField(max_length=50 ,default="새로운 메모")
     text = models.TextField(null=True, blank=True)
     is_cal = models.BooleanField(default=False)
     is_todo = models.BooleanField(default=False)
