4656df5c38df08351a4ae0164b8160675a5673b5
NohSungwoo <shtjddn0817@gmail.com>
2024-11-01 10:20:28 +0900

    feat: Add user model PLANA-34

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from common.models import CommonModel
 
-# Create your models here.
+
+class User(AbstractUser, CommonModel):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    email = models.EmailField(unique=True)
+    username = None
+    first_name = None
+    last_name = None
+    nickname = models.CharField(max_length=30)
+    gender = models.CharField(max_length=20 ,choices=GenderChoices)
+    birthday = models.PositiveIntegerField()
+    photo = models.URLField(null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
