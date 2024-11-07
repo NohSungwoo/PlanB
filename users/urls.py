f28bd3357ed3037941c7ef6a8d1b7cc7c4bc9307
NohSungwoo <shtjddn0817@gmail.com>
2024-11-07 17:08:44 +0900

    Revert "feat: Add Login PLANA-65"
    This reverts commit 0595b11e

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from users.views import Login, SignUp
+from users.views import SignUp
 
 urlpatterns = [
     path("signup/", SignUp.as_view()),
-    path("login/", Login.as_view()),
 ]
