2e158a4afee5fe7cd457862a179afc1dc6031d60
ChoiWheatley <chltmdgus604@naver.com>
2024-11-21 23:13:40 +0900

    feat: Create TestAuthBase for user auth

--- memos/tests.py
+++ memos/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from common.tests import TestAuthBase
+from memos.models import Memo, MemoSet
+
+User = get_user_model()
+
+
+class TestMemoList(TestAuthBase):
+
+    URL = "/api/v1/memos/"
+
+    def setUp(self):
+        super().setUp()
+
+        # create a test memoset
+
+        self.memo_set = MemoSet.objects.create(user=self.user, name="TestMemoSet")
+
+        # create a test Memo
+
+        self.memo = Memo.objects.create(
+            memo_set=self.memo_set,
+            content="Test Memo Content",
+        )
+
+
+class TestMemoDetail(TestAuthBase):
+    pass
+
+
+class TestMemoSetList(TestAuthBase):
+    pass
+
+
+class TestMemoSetDetail(TestAuthBase):
+    pass
