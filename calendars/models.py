7c4c9d3ca0b8ee43a59e58854ea3c0be9079ef3a
NohSungwoo <shtjddn0817@gmail.com>
2024-11-23 11:37:09 +0900

    fix: Schedule Model Update

--- calendars/models.py
+++ calendars/models.py
@@ -21,7 +21,7 @@ class Schedule(CommonModel):
     )
     participant = models.ManyToManyField("users.User", related_name="user_schedule")
     memo = models.OneToOneField(
-        "memos.Memo", on_delete=models.CASCADE, related_name="memo_schedule"
+        "memos.Memo", on_delete=models.CASCADE, related_name="memo_schedule", null=True
     )
     title = models.CharField(max_length=50)
     start_date = models.DateField()
